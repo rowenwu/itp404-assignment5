04c4f3eaf3328fceacce127397486d3d004dd574
Rowen Wu <kathertw@usc.edu>
2017-10-31 14:41:28 -0700

    sorted posts

--- app/controllers/subreddits.js
+++ app/controllers/subreddits.js
@@ -1,5 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    sortedPosts: Ember.computed('model', function() {
+        let posts = this.get('model');
 
+        let sortedPosts = posts.sort(function(postA, postB) {
+            if (postA.data.score < postB.data.score) {
+              return 1;
+            }
+            if (postA.data.score > postB.data.score) {
+              return -1;
+            }
+            return 0;
+          });
+      
+          return sortedPosts;
+    })
+        
 });
