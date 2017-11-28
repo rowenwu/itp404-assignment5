946947d7dfc17a4fcebceb41643c03929b8a7e78
Rowen Wu <kathertw@usc.edu>
2017-11-27 23:29:31 -0800

    fix

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,7 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   actions: {
     searchForSubreddits() {
-      this.transitionToRoute('subreddits', {subreddit_name: this.get('keywords')});
+      this.transitionToRoute('subreddits', this.get('keywords'));
     }
   }
 });
